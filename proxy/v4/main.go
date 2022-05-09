c3d6b1db9aa95291eea14863cdd5e86ef692c310
hanyi <yi_han@bupt.edu.cn>
2022-05-09 22:36:56 +0800

    commit

--- proxy/v4/main.go
+++ proxy/v4/main.go
@@ -18,7 +18,7 @@ const atypeHOST = 0x03
 const atypeIPV6 = 0x04
 
 func main() {
-	server, err := net.Listen("tcp", "127.0.0.1:1080")
+	server, err := net.Listen("tcp", "0.0.0.0:1080")
 	if err != nil {
 		panic(err)
 	}
